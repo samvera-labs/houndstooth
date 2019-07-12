fc096cad8bc9a825c1db2312d8f86cb971f2131a
Julie Allinson <julie@notch8.com>
2019-07-12 19:13:12 +0100

    addendum to return a true exist code in validate.rb, and add a Gemfile

---
+++ Gemfile
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+gem 'json_schemer'
+gem 'yamllint'
--- validate.rb
+++ validate.rb
@@ -1,4 +1,6 @@
-require "json_schemer"
+# frozen_string_literal: true
+
+require 'json_schemer'
 require 'pathname'
 
 @schema = Pathname.new('m3_json_schema.json')
@@ -9,28 +11,38 @@ exit unless File.exist?(ARGV[0])
 @specification = YAML.load_file(ARGV[0])
 
 def load_schema
-  begin
-    @schemer = JSONSchemer.schema(@schema)
-    puts "Schema valid? true"
-    true
-  rescue => e
-    puts e.message
-    puts "Schema valid? false"
-  end
+  @schemer = JSONSchemer.schema(@schema)
+  puts 'Schema valid? true'
+  true
+rescue StandardError => e
+  puts e.message
+  puts 'Schema valid? false'
 end
 
 def validate!
-  puts "Specification valid? #{@schemer.valid?(@specification)}"
   @schemer.validate(@specification)
 end
 
-result = validate! if load_schema == true
+def valid?
+  valid = @schemer.valid?(@specification)
+  puts "Specification valid? #{valid}"
+  valid
+end
 
-result.to_a.each do | error |
-  puts "Data: #{error['data']}"
-  puts "Data pointer: #{error['data_pointer']}"
-  puts "Schema: #{error['schema']}"
-  puts "Schema pointer: #{error['schema_pointer']}"
-  puts "Type: #{error['type']}"
-  puts "\n"
-end unless result.nil?
+if load_schema
+  result = validate!
+  result&.to_a&.each do |error|
+    puts "Data: #{error['data']}"
+    puts "Data pointer: #{error['data_pointer']}"
+    puts "Schema: #{error['schema']}"
+    puts "Schema pointer: #{error['schema_pointer']}"
+    puts "Type: #{error['type']}"
+    puts "\n"
+  end
+end
+
+if valid?
+  exit(true)
+else
+  exit(0)
+end
